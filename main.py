7d9e54e56f7116da0b54b7a1dce4b695b389b692
Himanshu Lohaniwal <lohaniwalhimanshu@gmail.com>
2021-01-04 18:45:59 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+cel = float(input("Enter the Temperature (Celsius)to be converted in Fahrenheit (Enter only decimal number) : "))
+
+fah = cel * 1.8
+fah = fah + 32
+
+print(str(fah),'is the Farenheit of',str(cel),'degree celsius..')
